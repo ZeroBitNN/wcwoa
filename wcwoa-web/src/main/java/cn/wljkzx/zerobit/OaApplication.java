6126662faf79be328a13a758ab5dc4600c7f70ed
zerobit <leonngx@hotmail.com>
2018-10-12 18:26:21 +0800

    Initial commit

---
+++ wcwoa-web/src/main/java/cn/wljkzx/zerobit/OaApplication.java
@@ -0,0 +1,22 @@
+package cn.wljkzx.zerobit;/**
+ * @author ZeroBit
+ * @create 2018-10-12 18:14
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName OaApplication
+ * @Description TODO
+ * @Author ZeroBit
+ * @Date 2018/10/12
+ **/
+
+@SpringBootApplication
+public class OaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OaApplication.class, args);
+    }
+}
